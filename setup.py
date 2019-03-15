f3ae641e944fc3485b44a53b93f3680a2897961a
Александр Кварацхелия <akvarats@gmail.com>
2019-03-15 16:32:54 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='schema_docs',
+    version='0.1',
+    description='schema_docs',
+    long_description="",
+    author='Alexander Kvaratskheliya',
+    author_email='akvarats@gmail.com',
+    url='https://github.com/akvarats/schema_docs',
+    license="",
+    packages=find_packages(exclude=('tests', 'docs'))
+)
